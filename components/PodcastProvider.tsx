fb048f422d8625c346a292c662634226018f3161
Mathieu Wrzesien <mathieu.wrzesien@outlook.com>
2020-09-29 13:52:23 -0400

    optional chaining

--- components/PodcastProvider.tsx
+++ components/PodcastProvider.tsx
@@ -75,12 +75,12 @@ class PodcastProvider extends React.Component<Props, State> {
         items
       ).map((item) => {
         return {
-          title: item.getElementsByTagName('title')[0].textContent,
-          description: item.getElementsByTagName('description')[0].textContent,
-          src: item.getElementsByTagName('enclosure')[0].getAttribute('url'),
-          image: item.getElementsByTagName('itunes:image')[0].getAttribute('href'),
-          publishedDate: item.getElementsByTagName('pubDate')[0].textContent,
-          duration: parseInt(item.getElementsByTagName('itunes:duration')[0].textContent)
+          title: item.getElementsByTagName('title')[0]?.textContent,
+          description: item.getElementsByTagName('description')[0]?.textContent,
+          src: item.getElementsByTagName('enclosure')[0]?.getAttribute('url'),
+          image: item.getElementsByTagName('itunes:image')[0]?.getAttribute('href'),
+          publishedDate: item.getElementsByTagName('pubDate')[0]?.textContent,
+          duration: parseInt(item.getElementsByTagName('itunes:duration')[0]?.textContent)
         };
       });
       this.setState({ podcastEpisodes: podcastEpisodes });
